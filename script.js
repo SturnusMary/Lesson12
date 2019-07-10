0ad7449bef21936d70a0dca36bc0b1e40421a099
Maryna <50712899+SturnusMary@users.noreply.github.com>
2019-07-10 18:00:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+// #1
+var secondsInMinute = 60, minutesInHour = 60;
+var secondsInHour = secondsInMinute * minutesInHour;
+var numberOfSeconds =  secondsInHour * 3;
+console.log(numberOfSeconds);
+
+// #2
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+
+// #3
+var a = 10, b = 2, c = 5;
+console.log(a + b + c);
+
+// #4
+var a = 17, b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+console.log(result);
+
+// #5
+var name = 'Maryna';
+console.log('Hello, ' + name + '!');
+
+
